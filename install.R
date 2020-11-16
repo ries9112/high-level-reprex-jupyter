e9134e37c75325f26c246c5f360ea541eb1469e8
Riccardo Esclapon <ries9112@colorado.edu>
2020-11-15 18:41:21 -0700

    update install

--- install.R
+++ install.R
@@ -1,1 +1,9 @@
-install.packages("pacman")
+install.packages('pacman')
+pacman::p_load('pins', 'skimr', 'DT', 'httr', 'jsonlite', # Data Exploration 
+               'tidyverse', 'tsibble', 'anytime', # Data Prep
+               'ggTimeSeries', 'gifski', 'av', 'magick', 'ggthemes', 'plotly', # Visualization
+               'ggpubr', 'ggforce', 'gganimate', 'transformr', # Visualization continued
+               'caret', 'doParallel', 'parallel', 'xgboost', # Predictive Modeling
+               'gbm', 'deepnet', 'elasticnet', 'pls',  # Predictive Modeling continued
+               'hydroGOF', 'formattable', 'knitr', # Evaluate Model Performance
+               "kernlab","tsibble","fabletools","fable","feasts","urca") # Last ones for high-level version
